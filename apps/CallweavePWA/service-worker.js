638e5807224e0c0f05767bfd577b13ece7ae6cf5
Enrico Piovesan <enrico.piovesan10@gmail.com>
2026-09-14 08:48:22 -0600

    Force Callweave PWA asset refresh

--- apps/CallweavePWA/service-worker.js
+++ apps/CallweavePWA/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'callweave-presentation-v22';
+const CACHE = 'callweave-presentation-v24';
 const ASSETS = ['./', './index.html', './styles.css', './soundscape.css', './main.js', './web-recording.js', './manifest.webmanifest', './assets/listening-soundscape.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
@@ -6,7 +6,7 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    fetch(event.request)
+    fetch(event.request, { cache: 'no-store' })
       .then(response => {
         const cachedResponse = response.clone();
         caches.open(CACHE).then(cache => cache.put(event.request, cachedResponse));
